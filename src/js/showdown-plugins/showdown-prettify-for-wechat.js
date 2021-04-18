5e03b1cd7ac8f73ed6f51880b441f1ea798aa16c
knightyun <2386209384@qq.com>
2021-04-19 00:06:33 +0800

    [fix]: 修复代码粘贴到微信文章中的错乱问题

--- src/js/showdown-plugins/showdown-prettify-for-wechat.js
+++ src/js/showdown-plugins/showdown-prettify-for-wechat.js
@@ -4,11 +4,11 @@ showdown.extension('prettify', function () {
   return [{
     type:   'output',
     filter: function (source) {
-      return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (match, pre, codeClass) {
+      return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (_, pre, codeClass) {
         if (pre) {
-          return '<pre class="prettyprint linenums" style="font-size: 10px;line-height: 12px"><code' + codeClass + ' style="font-size: 10px;line-height: 12px">';
+          return '<pre class="prettyprint linenums"><code' + codeClass + '>';
         } else {
-          return ' <code class="prettyprint code-in-text"  style="font-size: 16px;line-height: 18px">';
+          return '<code class="prettyprint code-in-text">';
         }
       });
     }
